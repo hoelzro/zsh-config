f2bddac44f2d83c2f9e6eb6f6c5bfd502bc2f212
Rob Hoelz <rob@hoelz.ro>
2013-07-24 14:27:29 +0200

    Define a command not found handler
    This is to handle situation when my fingers are stupid and type things
    like "gitp ush" rather than "git push"

--- hooks.zsh
+++ hooks.zsh
@@ -1,3 +1,15 @@
 typeset -a chpwd_functions
 
 chpwd_functions[$(($#chpwd_functions + 1))]=ls_limited
+
+command_not_found_handler() {
+    local cmd=$1
+    if [[ $cmd =~ ^git ]]; then
+        local subcommand=$2
+        shift
+        shift
+        subcommand="${cmd#git}$subcommand"
+        exec git $subcommand "$@"
+    fi
+    return 127
+}
