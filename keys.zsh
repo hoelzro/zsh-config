d357cae239796f4593f362ca79747e1b90074692
Rob Hoelz <rob@hoelz.ro>
2014-05-20 09:42:22 -0500

    Bind Ctrl-X-E to edit current command line

--- keys.zsh
+++ keys.zsh
@@ -1,5 +1,9 @@
+autoload -U edit-command-line
+zle -N edit-command-line
+
 bindkey -e
 bindkey '^B' backward-word
 bindkey '^F' forward-word
 bindkey '^U' backward-kill-line
 bindkey ${terminfo[kdch1]} delete-char
+bindkey '^X^E' edit-command-line
