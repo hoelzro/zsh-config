930e1a880cfcc3c226c0b017f2ed10b55184d21d
Rob Hoelz <rob@hoelz.ro>
2017-08-19 09:00:35 -0500

    Enable incremental search
    And drop keybindings I no longer need

--- keys.zsh
+++ keys.zsh
@@ -2,8 +2,7 @@ autoload -U edit-command-line
 zle -N edit-command-line
 
 bindkey -v
-bindkey '^B' backward-word
-bindkey '^F' forward-word
+bindkey -M vicmd '/' history-incremental-search-backward
 bindkey '^U' backward-kill-line
 bindkey ${terminfo[kdch1]} delete-char
 bindkey '^X^E' edit-command-line
