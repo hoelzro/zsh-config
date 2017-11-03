94e58cc305c1c6d69353e1dd34ed3ad0a8f4de55
Rob Hoelz <rob@hoelz.ro>
2017-11-03 14:06:12 -0500

    Disable incremental search
    It's still available the old way, the new way just sucked

--- keys.zsh
+++ keys.zsh
@@ -2,7 +2,6 @@ autoload -U edit-command-line
 zle -N edit-command-line
 
 bindkey -v
-bindkey -M vicmd '/' history-incremental-search-backward
 bindkey '^U' backward-kill-line
 bindkey ${terminfo[kdch1]} delete-char
 bindkey '^X^E' edit-command-line
