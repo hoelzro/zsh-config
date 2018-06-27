0f11b5be2fdd079587fac1b4b5aa3c8b5c543504
Rob Hoelz <rob@hoelz.ro>
2018-06-27 11:08:19 -0500

    Don't punish me for fat-fingering "!4"
    ...when I meant "!$".  I never mean "!4".

--- keys.zsh
+++ keys.zsh
@@ -5,3 +5,12 @@ bindkey -v
 bindkey '^U' backward-kill-line
 bindkey ${terminfo[kdch1]} delete-char
 bindkey '^X^E' edit-command-line
+
+function _fat_finger_bang4_expand() {
+    LBUFFER=${LBUFFER/%\!4/\!$}
+    zle expand-or-complete
+}
+
+zle -N fat_finger_bang4_expand _fat_finger_bang4_expand
+
+bindkey -M viins '^I' fat_finger_bang4_expand
