b9c095c33c0885db6a8aebb5f21dee88d32f2ef0
Rob Hoelz <rob@hoelz.ro>
2018-09-22 19:44:40 -0500

    Substitute pacsearch for pacman -Ss
    I often reach for the former when I type the latter, so I figured I'd
    automate it.  I have other substitutions in mind, but I figured I'd
    prototype them with this first

--- keys.zsh
+++ keys.zsh
@@ -13,6 +13,27 @@ function _fat_finger_bang4_expand() {
     zle expand-or-complete
 }
 
+function _pacsearch_replace() {
+    local first_word
+
+    first_word=${${(z)LBUFFER}[1]}
+
+    if [[ "$first_word" == 'pacman' ]]; then
+        local num_words second_word
+
+        num_words=${#${(z)LBUFFER}}
+        second_word=${${(z)LBUFFER}[2]}
+
+        if [[ $num_words -eq 2 && "$second_word" == '-Ss' ]]; then
+            LBUFFER='pacsearch'
+        fi
+    fi
+
+    zle self-insert
+}
+
 zle -N fat_finger_bang4_expand _fat_finger_bang4_expand
+zle -N pacsearch_replace _pacsearch_replace
 
 bindkey -M custom '^I' fat_finger_bang4_expand
+bindkey -M custom ' ' pacsearch_replace
