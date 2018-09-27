500938baf2cffbb3c4b979c5dc537316c5494268
Rob Hoelz <rob@hoelz.ro>
2018-09-26 21:54:50 -0500

    Work around hardcoded vi search keymap behavior
    When you enter search mode using vi-history-search-backward or vi-history-search-forward,
    zsh, as of version 5.6.2, loads the main keymap and dispatches on
    command values, rather than executing the widgets bound to the keys
    themselves.  The dispatch table is limited to a very small number of
    commands, and since it's coded into the C implementation, it obviously
    doesn't include custom widgets such as pacsearch_replace.  If a command
    value zsh doesn't recognize is encountered, it's ignored - which caused
    my search usage not to allow typing since I wrote the pacsearch_replace
    thing.  This fix works around that behavior

--- keys.zsh
+++ keys.zsh
@@ -32,8 +32,24 @@ function _pacsearch_replace() {
     zle self-insert
 }
 
+function _remove_pacsearch_replace_and_search_backward() {
+    bindkey -M custom ' ' self-insert
+    zle vi-history-search-backward
+    bindkey -M custom ' ' pacsearch_replace
+}
+
+function _remove_pacsearch_replace_and_search_forward() {
+    bindkey -M custom ' ' self-insert
+    zle vi-history-search-forward
+    bindkey -M custom ' ' pacsearch_replace
+}
+
 zle -N fat_finger_bang4_expand _fat_finger_bang4_expand
 zle -N pacsearch_replace _pacsearch_replace
+zle -N remove_pacsearch_replace_and_search_backward _remove_pacsearch_replace_and_search_backward
+zle -N remove_pacsearch_replace_and_search_forward _remove_pacsearch_replace_and_search_forward
 
 bindkey -M custom '^I' fat_finger_bang4_expand
 bindkey -M custom ' ' pacsearch_replace
+bindkey -M vicmd '/' remove_pacsearch_replace_and_search_backward
+bindkey -M vicmd '?' remove_pacsearch_replace_and_search_forward
