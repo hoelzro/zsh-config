70a375d382246fe367b5f1a27abc533dfde48117
Rob Hoelz <rob@hoelz.ro>
2021-12-21 08:08:59 -0600

    try out fzf

--- keys.zsh
+++ keys.zsh
@@ -95,3 +95,27 @@ bindkey -M vicmd -r '^[[A'
 bindkey -M vicmd -r '^[[B'
 
 bindkey -M viopp "i'" select-in-shell-word
+
+if which fzf &>/dev/null ; then
+    __fzfcmd() {
+      [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
+        echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+    }
+    fzf-history-widget() {
+      local selected num
+      setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+      selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
+        FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+      local ret=$?
+      if [ -n "$selected" ]; then
+        num=$selected[1]
+        if [ -n "$num" ]; then
+          zle vi-fetch-history -n $num
+        fi
+      fi
+      zle reset-prompt
+      return $ret
+    }
+    zle     -N   fzf-history-widget
+    bindkey -M vicmd '/' fzf-history-widget
+fi
