0d105bab48f9acb99b550c772859332778ed87e3
Rob Hoelz <rob@hoelz.ro>
2022-03-20 08:10:57 -0500

    sqlite history: insert current timezone offset

--- hist-append.pl
+++ hist-append.pl
@@ -8,6 +8,7 @@ use experimental qw(signatures);
 use DBI;
 use File::Basename qw(dirname);
 use File::Path qw(make_path);
+use Time::Piece;
 
 exit unless $ENV{'HISTFILE'};
 
@@ -54,7 +55,7 @@ if($current_schema_version != $post_create_table_schema_version) {
 # XXX detect shell exit?
 
 my $insert_sth = $dbh->prepare(<<'END_SQL');
-INSERT INTO history (hostname, session_id, timestamp, history_id, cwd, entry) VALUES (:hostname, :session_id, :timestamp, :history_id, :cwd, :entry);
+INSERT INTO history (hostname, session_id, timestamp, tz_offset, history_id, cwd, entry) VALUES (:hostname, :session_id, :timestamp, :tz_offset, :history_id, :cwd, :entry);
 END_SQL
 
 my ( $hostname, $session_id, $timestamp, $history_id, $cwd, $entry ) = @ARGV;
@@ -64,6 +65,7 @@ chomp $entry;
 $insert_sth->bind_param(':hostname'   => $hostname);
 $insert_sth->bind_param(':session_id' => $session_id);
 $insert_sth->bind_param(':timestamp'  => $timestamp);
+$insert_sth->bind_param(':tz_offset'  => localtime->tzoffset);
 $insert_sth->bind_param(':history_id' => $history_id);
 $insert_sth->bind_param(':cwd'        => $cwd);
 $insert_sth->bind_param(':entry'      => $entry);
