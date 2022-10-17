3398d0eaa7f65a6aae1235f6b6f6bcbf0435c174
Rob Hoelz <rob@hoelz.ro>
2022-10-17 09:48:55 -0500

    fzf: set --scheme=history
    Restore pre-0.32 scoring algorithm

--- keys.zsh
+++ keys.zsh
@@ -105,7 +105,7 @@ if which fzf &>/dev/null ; then
       local selected num
       setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
       selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
-        FZF_DEFAULT_OPTS="--exact --height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+        FZF_DEFAULT_OPTS="--exact --height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --scheme=history --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
       local ret=$?
       if [ -n "$selected" ]; then
         num=$selected[1]
