2396c2b1818917402ae76b6f7ff7acead69c90bc
Rob Hoelz <rob@hoelz.ro>
2025-06-10 21:53:17 -0500

    Add ability to update history by OID

--- hist-update.pl
+++ hist-update.pl
@@ -6,10 +6,17 @@ use feature qw(say);
 use experimental qw(signatures);
 
 use DBI;
+use Getopt::Long;
 use File::Basename qw(dirname);
 use File::Path qw(make_path);
 
 exit unless $ENV{'HISTFILE'};
+
+my $target_oid;
+
+GetOptions(
+  'oid=i' => \$target_oid,
+) or die "unable to process command line";
 
 my ( $hostname, $session_id, $target_history_id, $current_time, $exit_status ) = @ARGV;
 
@@ -23,7 +30,20 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=$database", undef, undef, {
     PrintError => 0,
 }); # XXX don't freak out if you can't connect
 
-my $update_sth = $dbh->prepare(<<'END_SQL');
+if(defined($target_oid)) {
+  my $update_sth = $dbh->prepare(<<'END_SQL');
+UPDATE history SET duration = :current_time - timestamp, exit_status = :status
+WHERE oid = :oid
+END_SQL
+
+  # XXX bind types?
+  $update_sth->bind_param(':oid'          => $target_oid);
+  $update_sth->bind_param(':current_time' => $current_time);
+  $update_sth->bind_param(':status'       => $exit_status);
+
+  $update_sth->execute;
+} else {
+  my $update_sth = $dbh->prepare(<<'END_SQL');
 UPDATE history SET duration = :current_time - timestamp, exit_status = :status
 WHERE hostname   = :hostname
 AND   session_id = :session_id
@@ -32,11 +52,12 @@ AND   history_id = :history_id
 AND duration IS NULL
 END_SQL
 
-# XXX bind types?
-$update_sth->bind_param(':hostname'     => $hostname);
-$update_sth->bind_param(':session_id'   => $session_id);
-$update_sth->bind_param(':history_id'   => $target_history_id);
-$update_sth->bind_param(':current_time' => $current_time);
-$update_sth->bind_param(':status'       => $exit_status);
+  # XXX bind types?
+  $update_sth->bind_param(':hostname'     => $hostname);
+  $update_sth->bind_param(':session_id'   => $session_id);
+  $update_sth->bind_param(':history_id'   => $target_history_id);
+  $update_sth->bind_param(':current_time' => $current_time);
+  $update_sth->bind_param(':status'       => $exit_status);
 
-$update_sth->execute;
+  $update_sth->execute;
+}
